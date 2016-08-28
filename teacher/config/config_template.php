d39c2eebf5223c2a7bc931eebbcffff5c7f9394f
ThinhNK <ThinhNguyenKhac@admicro.vn>
2016-08-28 14:32:53 +0700

    delete

--- teacher/config/config_template.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['template_f'] = '';
-$config['template_default'] = 'template/default/';
--- teacher/language/en/home_lang.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$lang['home_lang'] = array();
-$lang['home_lang']['title'] = 'Title';
